ce76570d496267a5b4ae7e40ff46cc703bb7625e
jy-z-cheng <zhangyaocheng24@gmail.com>
2014-08-31 01:03:18 -0400

    Added simple file read write for garage door flag

--- server_nodejs/server.js
+++ server_nodejs/server.js
@@ -1,23 +1,45 @@
 var http = require('http');
 var url = require('url');
+var queryString = require( "querystring" );
+var fs = require('fs');
+
+var filename = "/data.txt";
 
 var handlePost = function (request, response) {
 	if (request.url == "/") {
 		var parsedUrl = url.parse(request.url, true);
 		
+		fs.writeFile(filename, queryUrl.query, function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log("The file was saved!");
+			}
+		});
 	}
 }
 
 var handleGet = function (request, response) {
 	if (request.url == "/") {
 		
+		//var queryObj = queryString.parse ( parsedUrl.query );
+		
+		//var obj = JSON.parse( queryObj.jsonData );
+		
+		resp.writeHead(200, {"Content-Type":"text/html"});
+		fs.readFile(filename, "utf8", function (err, data) {
+			if (err) throw err;
+			resp.write(data);
+			resp.end();
+		});
+		
 	} else if (request.url == "/test") {
 		response.writeHead(200, { 'content-type': 'text/plain' });
-		response.write("I am alive.");
+		response.write("TESTING: I am alive.");
 		response.end();
 	} else {
 		response.writeHead(200, { 'content-type': 'text/plain' });
-		response.write("Wrong directory");
+		response.write("Wrong directory at " + request.url);
 		response.end();
 		console.log(request.url);
 	}
