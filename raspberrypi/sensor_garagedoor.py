2f32f3be89a209930e334fbff4b32e607ad3925f
jy-z-cheng <zhangyaocheng24@gmail.com>
2014-10-01 19:10:38 -0400

    Refractoring the code to have more fns
    Read state and post json functions

--- raspberrypi/sensor_garagedoor.py
+++ raspberrypi/sensor_garagedoor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import serial
 import json
 import requests
@@ -5,34 +7,44 @@ import requests
 STATUS_STRING = "status"
 STATUSCODE_STRING = "statuscode"
 
-prevState = None
-currState = None
-isOpen = None
-
-url = "http://192.168.0.37:3639/"
+post_url = "http://192.168.0.37:3639/"
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-while 1 : 
-  prevState = isOpen
 
-  if "0" in ser.readline(): # open
-    isOpen = False
-    currState = 0
-  elif '1' in ser.readline(): # close
-    isOpen = True
-    currState = 1
-  elif '2' in ser.readline(): # opening/closing
-    currState = 2
-  elif '41' in ser.readline(): # error occurred at arduino
-    currState = 41
+prev_state = None
+curr_state = None
+is_open = None
+
+def read_state(line):
+  if "0" in line: # open
+    return 0
+  elif '1' in line: # close
+    return 1
+  elif '2' in line: # opening/closing
+    return 2
+  elif '41' in line: # error occurred at arduino
+    return 41
   else:                       # error occurred at raspberrypi/python
-    currState = 42
+    return 42
 
-  if isOpen != prevState:
-    print("Changing flag")
-    headers = {'content-type': 'application/json'}
-    output = {STATUS_STRING: isOpen}
-    r = requests.post(url, data=json.dumps(output), headers=headers)
-    print("Flag changed")
+def post_json(url, output):
+  json_headers = {'content-type': 'application/json'}
+  return requests.post(url, data=json.dumps(output), headers=json_headers)
+
+while 1 : 
+  prev_state = curr_state
+  curr_state = read_state(ser.readline())
+
+  if curr_state == 0:
+    is_open = False
+  elif curr_state == 1:
+    is_open = True
+
+  if curr_state != prev_state:
+    print curr_state
+    # print("Changing flag")
+    post_data = {STATUS_STRING: is_open, STATUSCODE_STRING: curr_state}
+    r = post_json(post_url, post_data)
+    # print("Flag changed")
 
