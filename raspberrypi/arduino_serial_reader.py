72216be9f662413868b1151bdb55334e1431590a
jy-z-cheng <zhangyaocheng24@gmail.com>
2014-10-01 20:33:35 -0400

    Changed indentation from 2 spaces to 4 spaces

--- raspberrypi/arduino_serial_reader.py
+++ raspberrypi/arduino_serial_reader.py
@@ -17,34 +17,34 @@ curr_state = None
 is_open = None
 
 def read_state(line):
-  if "0" in line: # open
-    return 0
-  elif '1' in line: # close
-    return 1
-  elif '2' in line: # opening/closing
-    return 2
-  elif '41' in line: # error occurred at arduino
-    return 41
-  else:                       # error occurred at raspberrypi/python
-    return 42
+    if "0" in line: # open
+        return 0
+    elif '1' in line: # close
+        return 1
+    elif '2' in line: # opening/closing
+        return 2
+    elif '41' in line: # error occurred at arduino
+        return 41
+    else:                                             # error occurred at raspberrypi/python
+        return 42
 
 def post_json(url, output):
-  json_headers = {'content-type': 'application/json'}
-  return requests.post(url, data=json.dumps(output), headers=json_headers)
+    json_headers = {'content-type': 'application/json'}
+    return requests.post(url, data=json.dumps(output), headers=json_headers)
 
 while 1 : 
-  prev_state = curr_state
-  curr_state = read_state(ser.readline())
+    prev_state = curr_state
+    curr_state = read_state(ser.readline())
 
-  if curr_state == 0:
-    is_open = False
-  elif curr_state == 1:
-    is_open = True
+    if curr_state == 0:
+        is_open = False
+    elif curr_state == 1:
+        is_open = True
 
-  if curr_state != prev_state:
-    print curr_state
-    # print("Changing flag")
-    post_data = {STATUS_STRING: is_open, STATUSCODE_STRING: curr_state}
-    r = post_json(post_url, post_data)
-    # print("Flag changed")
+    if curr_state != prev_state:
+        print curr_state
+        # print("Changing flag")
+        post_data = {STATUS_STRING: is_open, STATUSCODE_STRING: curr_state}
+        r = post_json(post_url, post_data)
+        # print("Flag changed")
 
